d50cfba2f4c1b1a5b90ec165c51663ee4f6c9dd7
ruslanepi <ruslanep2@gmail.com>
2022-04-21 22:22:19 +0300

    added list type, event types

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card, { CardVariant } from './components/Card'
-import UserList from './components/UserList'
-import { IUser } from './types/types'
+
+import { ITodo, IUser } from './types/types'
+import axios from 'axios'
+import List from './components/List'
+import UserItem from './components/UserItem'
+import TodoItem from './components/TodoItem'
+import EventsExample from './components/EventsExample'
 
 const App = () => {
-  const users: IUser[] = [
-    {
-      id: 1,
-      name: 'Ruslan',
-      email: '123@mail.ru',
-      address: { city: 'kazan', street: 'Zur Uram', zipcode: '123123' },
-    },
-    {
-      id: 2,
-      name: 'Sveta',
-      email: '1234@mail.ru',
-      address: { city: 'kazan', street: 'Zur Uram', zipcode: '123123' },
-    },
-  ]
+  const [users, setUsers] = useState<IUser[]>([])
+  const [todos, setTodos] = useState<ITodo[]>([])
+
+  useEffect(() => {
+    fetchUsers()
+    fetchTodos()
+  }, [])
+
+  async function fetchUsers() {
+    try {
+      const response = await axios.get<IUser[]>(
+        'https://jsonplaceholder.typicode.com/users'
+      )
+      setUsers(response.data)
+    } catch (e) {
+      alert(e)
+    }
+  }
+
+  async function fetchTodos() {
+    try {
+      const response = await axios.get<ITodo[]>(
+        'https://jsonplaceholder.typicode.com/todos?_limit=10'
+      )
+      setTodos(response.data)
+    } catch (e) {
+      alert(e)
+    }
+  }
 
   return (
     <div>
+      <EventsExample />
       <Card
         onClick={(num) => console.log('log!', num)}
         variant={CardVariant.outlined}
@@ -29,7 +50,15 @@ const App = () => {
       >
         <button>click</button>
       </Card>
-      <UserList users={users} />
+      <List
+        items={users}
+        renderItem={(user: IUser) => <UserItem user={user} key={user.id} />}
+      />
+
+      <List
+        items={todos}
+        renderItem={(todo: ITodo) => <TodoItem todo={todo} key={todo.id} />}
+      />
     </div>
   )
 }
---
+++ src/components/EventsExample.tsx
@@ -0,0 +1,68 @@
+import React, { FC, useRef, useState } from 'react'
+
+const EventsExample: FC = () => {
+  const [value, setValue] = useState<string>('')
+  const [isDrag, setIsDrag] = useState<boolean>(false)
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value)
+  }
+
+  const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(inputRef.current?.value)
+  }
+
+  const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    console.log('drag')
+  }
+
+  const dragWithPreventHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    setIsDrag(true)
+  }
+
+  const leaveHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    setIsDrag(false)
+  }
+
+  const dropHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    setIsDrag(false)
+    console.log('DROP')
+  }
+
+  return (
+    <div>
+      <input
+        value={value}
+        onChange={changeHandler}
+        type='text'
+        placeholder='Управляемый'
+      />
+      <input ref={inputRef} type='text' placeholder='Неуправляемый' />
+
+      <button onClick={clickHandler}>click </button>
+
+      <div
+        draggable
+        onDrag={dragHandler}
+        style={{ width: 300, height: 200, background: 'red' }}
+      ></div>
+      <div
+        onDrop={dropHandler}
+        onDragLeave={leaveHandler}
+        onDragOver={dragWithPreventHandler}
+        style={{
+          width: 300,
+          height: 200,
+          background: isDrag ? 'blue' : 'red',
+          marginTop: 15,
+        }}
+      ></div>
+    </div>
+  )
+}
+
+export default EventsExample
